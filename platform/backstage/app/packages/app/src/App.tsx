df8738fa542aebbe79977f9df02129ae12181f93
Daniel Weber <daniel.weber@merckgroup.com>
2026-06-12 09:54:53 +0200

    added github auth

--- platform/backstage/app/packages/app/src/App.tsx
+++ platform/backstage/app/packages/app/src/App.tsx
@@ -3,6 +3,36 @@ import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
 
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      (
+        <SignInPage
+          {...props}
+          provider={{
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          }}
+        />
+      ),
+  },
+});
+
 export default createApp({
-  features: [catalogPlugin, kubernetesPlugin, navModule],
+  features: [
+    catalogPlugin,
+    kubernetesPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
